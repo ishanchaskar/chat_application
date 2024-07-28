71d729de8865427f68fb94672577415b207d153f
ishanchaskar <91770647+ishanchaskar@users.noreply.github.com>
2024-07-28 17:02:19 +0530

    messages backend done

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -1,6 +1,51 @@
 const asyncHandler = require("express-async-handler");
+const Message = require("../models/messageModel");
+const Chat = require("../models/chatModel");
+const User = require("../models/userModel");
 
-const sendMessage = asyncHandler (async(req , res) =>{})
+const sendMessage = asyncHandler(async (req, res) => {
+    const { chatId, content } = req.body;
 
-const allMessage = asyncHandler (async(req , res) =>{})
-module.exports = {sendMessage , allMessage}
+    if (!chatId || !content) {
+        console.log("Invalid data passed into the request");
+        return res.status(400).send("Invalid data passed into the request");
+    }
+
+    var newMessage = {
+        sender: req.user._id,
+        content: content,
+        chat: chatId,
+    };
+
+    try {
+        var message = await Message.create(newMessage);
+        message = await message.populate("sender", "name pic");
+        message = await message.populate("chat");
+        message = await User.populate(message, {
+            path: "chat.users",
+            select: "name pic email"
+        });
+
+        await Chat.findByIdAndUpdate(req.body.chatId, {
+            latestMessage: message,
+        });
+
+        res.json(message);
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
+
+const allMessage = asyncHandler(async (req, res) => {
+    try {
+        const messages = await Message.find({chat : req.params.chatId}).populate("sender" , "name pic email ")
+        .populate("chat")
+        res.json(messages)
+    } catch (error) {
+        res.status(400)
+        throw new Error(error.message)
+    }
+});
+
+module.exports = { sendMessage, allMessage };

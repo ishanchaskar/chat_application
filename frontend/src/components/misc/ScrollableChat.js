3170f9d6cadf8b39a5464ac2ab7861c3f2a9045e
ishanchaskar <91770647+ishanchaskar@users.noreply.github.com>
2024-07-30 22:23:22 +0530

    avatar added

--- frontend/src/components/misc/ScrollableChat.js
+++ frontend/src/components/misc/ScrollableChat.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import ScrollableFeed from "react-scrollable-feed"
+import {ChatState} from "../../Context/ChatProvider"
+import { isLastMessage, isSameSender } from '../../config/ChatLogic'
+import { Avatar, Tooltip } from '@chakra-ui/react'
 const ScrollableChat = ({messages}) => {
+  const {user} = ChatState()
   return (
     <><ScrollableFeed>
         {messages && messages.map((m , i) =>(
-            <div style={{display: "flex"}} key={m._id}></div>
+            <div style={{display: "flex"}} key={m._id}>
+
+              {(isSameSender(messages , m , i ,user._id) || isLastMessage(messages , i , user._id)) && (
+              <Tooltip
+              label={m.sender.name}
+              placement="bottom-start"
+              hasArrow
+              >
+                <Avatar
+                mt="7px"
+                mr={1}
+                size="sm"
+                cursor="pointer"
+                name ={m.sender.name}
+                src={m.sender.pic}
+                />
+              </Tooltip>
+              )}
+              <span style={{
+                backgroundColor: `${
+                  m.sender._id === user._id ?  "#BEE3F8" : "#B9F5D0"
+                }`,
+                borderRadius: "20px",
+                padding:"5px 15px",
+                maxWidth:"75%"
+              }}>{m.content}</span>
+            </div>
         ))}
         </ScrollableFeed></>
   )
